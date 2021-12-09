dbd2736fce4028ab5762622af1717d9285136719
farizaiis <farizaiis@hotmail.com>
2021-12-09 11:14:25 +0700

    logic test

--- farizAfif_ReadingBook/farizAfif_ReadingBook.js
+++ farizAfif_ReadingBook/farizAfif_ReadingBook.js
@@ -6,8 +6,32 @@ class Book {
     }
 }
 
-const totalChosenBooks = 4;
-const listBook = [
+function solve(totalChosenBooks, listBook) {
+    let result = 0;
+    listBook.map((v) => {
+        if (v.alice == 1 && v.bob == 1) {
+            result = result + v.time;
+        }
+    });
+
+    if (result < totalChosenBooks) {
+        return -1;
+    }
+
+    return result;
+}
+class Main {
+    constructor(totalChosenBooks, listBook) {
+        this.totalChosenBooks = totalChosenBooks;
+        this.listBook = listBook;
+    }
+    getResult() {
+        return solve(this.totalChosenBooks, this.listBook);
+    }
+}
+
+const total = 4;
+const array = [
     new Book(7, 1, 1),
     new Book(2, 1, 1),
     new Book(4, 0, 1),
@@ -18,18 +42,6 @@ const listBook = [
     new Book(3, 0, 0),
 ];
 
+const finalResult = new Main(total, array);
 
-function solve(totalChosenBooks, listBook) {
-    let result = 0;
-    listBook.map((v) => {
-        if (v.alice == 1 && v.bob == 1) {
-            result = result + v.time;
-        }
-    });
-
-    if(result < totalChosenBooks) {
-        return -1
-    }
-
-    return result
-}
+console.log(finalResult.getResult());
